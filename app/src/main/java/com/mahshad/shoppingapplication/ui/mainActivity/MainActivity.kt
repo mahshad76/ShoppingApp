79c732b06425694047f38fac61f5201131565a33
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-04-13 15:12:22 -0400

    Bottom navigation bar with two items

--- app/src/main/java/com/mahshad/shoppingapplication/ui/mainActivity/MainActivity.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/ui/mainActivity/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.mahshad.shoppingapplication.ui.mainActivity
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mahshad.shoppingapplication.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.main_activity)
-    }
-}
+
+        // Find the BottomNavigationView (optional, but good practice)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+}}
