072e8e6eda319cef0b9f4170a1713181f92d702b
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-04-14 17:46:48 -0400

    dto is added

---
+++ app/src/main/java/com/mahshad/shoppingapplication/dto/ProductsItem.kt
@@ -0,0 +1,11 @@
+package com.mahshad.shoppingapplication.dto
+
+data class ProductsItem(
+    val category: String,
+    val description: String,
+    val id: Int,
+    val image: String,
+    val price: Double,
+    val rating: Rating,
+    val title: String
+)
---
+++ app/src/main/java/com/mahshad/shoppingapplication/dto/Rating.kt
@@ -0,0 +1,6 @@
+package com.mahshad.shoppingapplication.dto
+
+data class Rating(
+    val count: Int,
+    val rate: Double
+)
