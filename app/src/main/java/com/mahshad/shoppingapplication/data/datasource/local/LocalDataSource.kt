fdb989fd7da6177ea2b5aecbf63f37c19dcbb0a2
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-04-26 18:35:09 -0400

    interface including the functions for modifying the shared preferences

--- app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/LocalDataSource.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/LocalDataSource.kt
@@ -1,4 +1,13 @@
 package com.mahshad.shoppingapplication.data.datasource.local
 
+/*
+Keeping the username and password in a shared preference file for the shopping application.
+ */
 interface LocalDataSource {
+
+    fun saveUser(key: String, str: String)
+
+    fun getPassword(key: String): String
+
+    fun clearUser(key: String)
 }
--- app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
@@ -3,5 +3,6 @@ package com.mahshad.shoppingapplication.data.datasource.local
 import android.content.Context
 import javax.inject.Inject
 
+///There are just three categories so
 class SharedPrefLocalDataSource @Inject constructor(context: Context) : LocalDataSource {
 }
