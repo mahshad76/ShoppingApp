df1d00a20bdf3507c455f56869c0bd5fb0270560
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-04-26 21:19:02 -0400

    shared local data source

--- app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
@@ -1,8 +1,22 @@
 package com.mahshad.shoppingapplication.data.datasource.local
 
 import android.content.Context
+import androidx.core.content.edit
 import javax.inject.Inject
 
 ///There are just three categories so
 class SharedPrefLocalDataSource @Inject constructor(context: Context) : LocalDataSource {
+    private val sharedPref = context.getSharedPreferences("users_info", Context.MODE_PRIVATE)
+
+    override fun saveUser(key: String, str: String) {
+        sharedPref.edit { putString(key, str) }
+    }
+
+    override fun getPassword(key: String): String {
+        return sharedPref.getString(key, null) ?: ""
+    }
+
+    override fun clearUser(key: String) {
+        sharedPref.edit { remove(key) }
+    }
 }
