46a4a7cd081b00221e69622326304f9d97515d4b
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-05-02 12:16:38 -0400

    updated the SharedPrefLocalDataSource with Rxjava

--- app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/data/datasource/local/SharedPrefLocalDataSource.kt
@@ -2,21 +2,44 @@ package com.mahshad.shoppingapplication.data.datasource.local
 
 import android.content.Context
 import androidx.core.content.edit
+import com.mahshad.shoppingapplication.di.IoScheduler
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Scheduler
 import javax.inject.Inject
 
-///There are just three categories so
-class SharedPrefLocalDataSource @Inject constructor(context: Context) : LocalDataSource {
-    private val sharedPref = context.getSharedPreferences("users_info", Context.MODE_PRIVATE)
+private const val USER_INFO = "user_info"
 
-    override fun saveUser(key: String, str: String) {
-        sharedPref.edit { putString(key, str) }
+class SharedPrefLocalDataSource @Inject constructor(
+    context: Context,
+    @IoScheduler private val ioScheduler: Scheduler
+) : LocalDataSource {
+    private val sharedPref = context.getSharedPreferences(USER_INFO, Context.MODE_PRIVATE)
+
+    override fun saveUser(key: String, str: String): Completable {
+        //fromCallable
+        return Completable.create { emitter ->
+            try {
+                sharedPref.edit { putString(key, str) }
+                emitter.onComplete()
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+        }.subscribeOn(ioScheduler)
     }
 
-    override fun getPassword(key: String): String {
-        return sharedPref.getString(key, null) ?: ""
+    override fun getPassword(key: String): Maybe<String?> {
+        return Maybe.create { emitter ->
+            emitter.onSuccess(sharedPref.getString(key, ""))
+            //emitter.onError()
+            sharedPref.getString(key, "")
+        }.filter { it.isNotEmpty() }
+            .subscribeOn(ioScheduler)
     }
 
-    override fun clearUser(key: String) {
-        sharedPref.edit { remove(key) }
+    override fun clearUser(key: String): Completable {
+        return Completable.create { emitter ->
+            sharedPref.edit { remove(key) }
+        }.subscribeOn(ioScheduler)
     }
 }
