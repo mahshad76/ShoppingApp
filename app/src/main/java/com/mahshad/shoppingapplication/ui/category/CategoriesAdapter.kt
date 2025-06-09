9831a382c330f44ff5343ce8d3b2d509bfe0cc65
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-09 11:56:46 -0400

    adaptor class for the categories fragment is implemented

--- app/src/main/java/com/mahshad/shoppingapplication/ui/category/CategoriesAdapter.kt
+++ app/src/main/java/com/mahshad/shoppingapplication/ui/category/CategoriesAdapter.kt
@@ -1,6 +1,28 @@
 package com.mahshad.shoppingapplication.ui.category
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.mahshad.shoppingapplication.R
 
-class CategoriesAdapter: Adapter<> {
+class CategoriesAdapter(private val categoriesList: List<String>) : Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.category_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = categoriesList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = categoriesList[position]
+    }
+}
+
+class ViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val textView: TextView = itemView.findViewById(R.id.categoryTextView)
 }
