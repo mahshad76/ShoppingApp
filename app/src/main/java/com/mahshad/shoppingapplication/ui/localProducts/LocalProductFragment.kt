22b8287249079d382b36eda62d0da2acba9fb372
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-11 17:19:06 -0400

    new fragment without access to the db

---
+++ app/src/main/java/com/mahshad/shoppingapplication/ui/localProducts/LocalProductFragment.kt
@@ -0,0 +1,22 @@
+package com.mahshad.shoppingapplication.ui.localProducts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mahshad.shoppingapplication.R
+
+class LocalProductFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_local_product, container, false)
+    }
+}
